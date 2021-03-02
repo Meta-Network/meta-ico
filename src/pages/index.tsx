efa762c400bc7dba8e71c6123c1ad782512fd83b
Frank Wei <frank@frankwei.xyz>
2021-03-02 20:41:05 +0800

    fake page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,43 @@
 import Head from 'next/head'
 import Link from "next/link"
 import { useWallet } from "use-wallet";
+import { Flex, Button, Spacer, Box, Text, CircularProgress, CircularProgressLabel, useDisclosure } from "@chakra-ui/react";
 import styles from '../styles/Home.module.css'
-
+import { ConnectWalletModal } from "../components/ConnectWalletModal";
 export default function Home() {
-  const isDevelopment = process.env.NODE_ENV === 'development';
+    const { isOpen, onOpen, onClose } = useDisclosure()
   const wallet = useWallet()
+  const MetaTotalSupply = 1145141919810
+  const metaSold = 191981000000
 
   return (
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Meta Network
+        Meta Network
         </h1>
-        {wallet.status === 'connected' ? wallet.account : 'Not Connected'}
-        <Link href="/connect">Connect Wallet</Link>
-        <p className={styles.description}>
-          {
-            isDevelopment
-              ? <>
-              Get started by editing{' '}
-              <code className={styles.code}>pages/index.js</code>
-              </>
-              : <>Coming Soon</>
-          }
-          
-        </p>
+      <Flex>
+  <Box p="4">
+    <CircularProgress value={40} size="128px" color="green.400">
+            <CircularProgressLabel>{ (metaSold / MetaTotalSupply * 100).toFixed(2) }%</CircularProgressLabel>
+          </CircularProgress>
+          <p>META have been distributed</p>
+  </Box>
+  <Spacer />
+  <Box p="4">
+          <Text fontSize="3xl">META Global Distribution</Text>
+          <Text>{metaSold} META Sold</Text>
+          <Text>{MetaTotalSupply-metaSold} META Left</Text>
+          <Text>11:45:14 Remaining</Text>
+          {wallet.status !== 'connected'
+            ? <Button onClick={onOpen}>Connect wallet</Button>
+            : <>
+              <Text>👛 {wallet.account}</Text>
+              <Text>Connect by { wallet.connector }</Text>
+          <Button colorScheme="red">Just take my money</Button></>}
+  </Box>
+      </Flex>
+          <ConnectWalletModal isOpen={isOpen} onClose={onClose} />
+
       </main>
   )
 }
