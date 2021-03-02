805d36fca5608586eeb4840ed647dd750726507c
Frank Wei <frank@frankwei.xyz>
2021-03-02 15:59:24 +0800

    ✨ display wallet status in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import styles from '../styles/navbar.module.css'
+import { useWallet } from "use-wallet";
+import { getShortedAddress } from '../utils';
 
 export function Navbar() {
+  const wallet = useWallet()
     return <section className="navbar site-section no-margin">
       <section className="section-container">
         <h1 className="logo">META Network</h1>
         <nav className={styles['navbar-nav']}>
           <a href="#">Blog</a>
           <a href="#">GitHub</a>
+          {wallet.status === 'connected' ? <>
+            <span>👛 {getShortedAddress(wallet.account)}</span>
+            <button onClick={() => wallet.reset()}>Disconnect</button>
+            </> : <button>Connect</button>}
       </nav>
       </section>
     </section>
