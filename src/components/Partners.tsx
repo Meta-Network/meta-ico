a0cb26737e48d91f297211172a0e1e37eaa41bcf
Frank Wei <frank@frankwei.xyz>
2021-03-03 13:35:22 +0800

    fix: better layout in the PartnersDisplay

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,23 +1,37 @@
-import { Box, Flex, Image, ImageProps, Heading } from "@chakra-ui/react";
+import { Box, Wrap, WrapItem, Image, ImageProps, Heading } from "@chakra-ui/react";
 
 const LogoProps: ImageProps = {
-    height: "100px",
+    height: "40px",
     filter: "grayscale(100%)",
     margin: '1'
 }
 
+// Better use external CDN to store these logos
+const logosUrls: string[] = [
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    // Just test for the layout
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+    "https://raw.githubusercontent.com/ethglobal/sponsor-logos/master/arweave.svg",
+    "https://polygon.technology/wp-content/uploads/2021/01/logo-polygon.png",
+]
+
 export function PartnersDisplay() {
     return <Box>
         <Heading textAlign="center">Partner & Supporter </Heading>
-        <Flex>
-            <Image  {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        <Image {...LogoProps} src="https://bit.ly/sage-adebayo" />
-        </Flex>
+        <Wrap justify="center" spacing="2rem">
+            { logosUrls.map((logo, idx) => <WrapItem key={idx}>
+                <Image  {...LogoProps} src={logo} />
+            </WrapItem>) }
+        </Wrap>
     </Box>
 }
