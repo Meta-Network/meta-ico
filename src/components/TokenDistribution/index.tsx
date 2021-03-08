0a8cd9c103871de8203340a8dd6c28d9ee054b44
Garfield Lee <openpgp@550.moe>
2021-03-08 12:44:58 +0800

    pref: better styles

--- src/components/TokenDistribution/index.tsx
+++ src/components/TokenDistribution/index.tsx
@@ -1,8 +1,8 @@
-import { Flex, Box, Button, FlexProps, useDisclosure, Text, TextProps, Spacer, Stack } from "@chakra-ui/react";
+import { Flex, Box, Button, FlexProps, useDisclosure, Text, TextProps, Spacer, Stack, ButtonProps } from "@chakra-ui/react";
 import { Progress } from "./compoents/CustomProgress";
 import { useWallet } from "use-wallet";
 import MetaCoinLogo from "../../../public/assets/logo_meta@2x.png";
-import { Image } from "@chakra-ui/react";
+import { Image, ImageProps } from "@chakra-ui/react";
 import { ConnectWalletModal } from "../ConnectWalletModal";
 
 const HeadTextProps: TextProps = {
@@ -26,8 +26,31 @@ const unitTextProps: TextProps = {
 }
 
 const TokenDistributionStyles: FlexProps = {
+    width: "1000px",
     margin: "0 auto 128px",
     fontFamily: "DINAlternate-Bold,DINAlternate"
+}
+
+const TitleText: TextProps = {
+    color: "#333333",
+    fontSize: "56px",
+    fontWeight: "bold",
+    lineHeight: "66px",
+    marginBottom: "24px"
+}
+
+const LogoImage: ImageProps = {
+    width: "256px",
+    height: "256px",
+    padding: "40px"
+}
+
+const RoundButton: ButtonProps = {
+    marginTop: 2,
+    rounded: 24,
+    width: "192px",
+    marginLeft: "auto",
+    marginRight: "auto"
 }
 
 export function TokenDistribution() {
@@ -37,43 +60,44 @@ export function TokenDistribution() {
     const percentage = (metaSold / metaTotalSupply * 100);
     const isNearlyEnd = percentage >= 90
     const wallet = useWallet()
-    return <Flex {...TokenDistributionStyles}>
-        <Box p="2" display='grid'>
-            <Image src={MetaCoinLogo} width={256} height={256} />
-          {wallet.status !== 'connected'
-            ? <Button onClick={onOpen} marginTop={2}>Connect wallet</Button>
-            : 
-              <Button colorScheme="purple" marginTop={2} rounded={24}>Get Meta Now</Button>}
-        </Box>
-        <Spacer />
-        <Box width={640}>
-          <Text fontSize="3xl" fontWeight="bold">META Token Distribution</Text>
-          <Text {...HeadTextProps}>TOTAL DISTRIBUTED</Text>
-            <Progress value={percentage} borderRadius={15} colorScheme="orange" />
-            <Flex>
-                <Text {...minorTextProps}>0 META</Text>
-                <Spacer />
-                <Text {...minorTextProps}>{metaTotalSupply} META</Text>
-            </Flex>
-            <Stack>
-                <Text {...HeadTextProps}>CURRENT DISTRIBUTION</Text>
+    return (
+        <Flex {...TokenDistributionStyles}>
+            <Box p="2" display='grid' justifyContent="center">
+                <Image {...LogoImage} src={MetaCoinLogo}  />
+                {wallet.status !== 'connected'
+                    ? <Button {...RoundButton} onClick={onOpen}>Connect wallet</Button>
+                    : <Button {...RoundButton} colorScheme="mttk">Get Meta Now!</Button>}
+            </Box>
+            <Spacer />
+            <Box width={640}>
+                <Text {...TitleText}>META Token Distribution</Text>
+                <Text {...HeadTextProps}>TOTAL DISTRIBUTED</Text>
+                <Progress value={percentage} borderRadius={15} colorScheme="orange" />
                 <Flex>
-                    <Flex alignItems="baseline">
-                        <Text {...NumberTextProps}>100000000</Text>
-                        <Text {...unitTextProps}>META</Text>
-                    </Flex>
+                    <Text {...minorTextProps}>0 META</Text>
                     <Spacer />
-                    <Flex alignItems="baseline">
-                        <Text {...NumberTextProps}>114514</Text>
-                        <Text  {...unitTextProps}>BNB Received</Text>
-                    </Flex>
+                    <Text {...minorTextProps}>{metaTotalSupply} META</Text>
                 </Flex>
-                <Box>
-                    <Text {...HeadTextProps}>CURRENT DISTRIBUTION ENDS IN</Text>
-                    <Text {...NumberTextProps}>11:45:14:55</Text>
-                </Box>
-            </Stack>
-        </Box>
-        <ConnectWalletModal isOpen={isOpen} onClose={onClose} />
-      </Flex>
+                <Stack>
+                    <Text {...HeadTextProps}>CURRENT DISTRIBUTION</Text>
+                    <Flex>
+                        <Flex alignItems="baseline">
+                            <Text {...NumberTextProps}>100000000</Text>
+                            <Text {...unitTextProps}>META</Text>
+                        </Flex>
+                        <Spacer />
+                        <Flex alignItems="baseline">
+                            <Text {...NumberTextProps}>114514</Text>
+                            <Text  {...unitTextProps}>BNB Received</Text>
+                        </Flex>
+                    </Flex>
+                    <Box>
+                        <Text {...HeadTextProps}>CURRENT DISTRIBUTION ENDS IN</Text>
+                        <Text {...NumberTextProps}>11:45:14:55</Text>
+                    </Box>
+                </Stack>
+            </Box>
+            <ConnectWalletModal isOpen={isOpen} onClose={onClose} />
+        </Flex>
+    )
 }
