c7f86fb2383033dafa4ebafbc1c36207bbf03ec3
Garfield Lee <openpgp@550.moe>
2021-03-29 18:43:17 +0800

    feat: add lots of logos

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -1,9 +1,18 @@
-import { Box, BoxProps, Wrap, WrapProps, WrapItem, Image, ImageProps, Heading, HeadingProps } from "@chakra-ui/react";
+import { Box, Wrap, WrapProps, WrapItem, Image, ImageProps } from "@chakra-ui/react";
 import LogoArweave from "../../assets/img/logo-arweave.svg";
 import LogoPolygon from "../../assets/img/logo-polygon.png";
 import LogoXOrder from "../../assets/img/logo-xorder.png";
 import LogoGitcoin from "../../assets/img/logo-gitcoin.png";
 import LogoLoop from "../../assets/img/logo-loop.png";
+import LogoNGCVentures from "../../assets/img/logo-ngc-ventures.png";
+import LogoCyberightCapital from "../../assets/img/logo-cyberight-capital.png";
+import Logo499DAOs from "../../assets/img/logo-499-daos.png";
+import LogoYFII from "../../assets/img/logo-yfii.png";
+import LogoMCDEX from "../../assets/img/logo-mcdex.png";
+import LogoDForce from "../../assets/img/logo-dforce.png";
+import LogoBinance from "../../assets/img/logo-binance.png";
+import LogoPrimitiveCrypto from "../../assets/img/logo-primitive-crypto.png";
+import LogoTernaryCaptial from "../../assets/img/logo-ternary-captial.png";
 
 import styles from './index.module.scss';
 
@@ -26,17 +35,26 @@ const logosUrls: string[] = [
     LogoPolygon,
     LogoXOrder,
     LogoGitcoin,
-    LogoLoop
+    LogoLoop,
+    LogoNGCVentures,
+    LogoCyberightCapital,
+    Logo499DAOs,
+    LogoYFII,
+    LogoMCDEX,
+    LogoDForce,
+    LogoBinance,
+    LogoPrimitiveCrypto,
+    LogoTernaryCaptial
 ]
 
 export function PartnersDisplay() {
     return (
         <Box className={ styles.wrapper }>
-            <h2 className={ `common-title ${styles.title}` }>Partner & Supporter </h2>
+            <h2 className={ `common-title ${styles.title}` }>Partner & Supporter</h2>
             <Wrap {...LogoWrap}>
                 {logosUrls.map((logo, idx) => (
                     <WrapItem key={idx}>
-                        <Image  {...LogoProps} src={logo} />
+                        <Image {...LogoProps} src={logo} alt='logo' />
                     </WrapItem>
                 ))}
             </Wrap>
